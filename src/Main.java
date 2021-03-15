ec274ecb60420b88abfc41bc481898e7f70d17f9
Sergei Lychagin <lychagin0@gmail.com>
2021-03-15 10:40:43 +0400

    форматирование_2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        float prise = 510.00f;      
-        int bonus = (int)prise / 20;
-        
-System.out.println("Your bonus is" + "  " + bonus + " " + "mil");
+        float prise = 510.00f;
+        int bonus = (int) prise / 20;
+
+        System.out.println("Your bonus is" + "  " + bonus + " " + "mil");
 
     }
 }
